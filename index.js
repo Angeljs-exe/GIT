3628b36e2b96d3328bb2edf1ab5ad725ab0291c2
Angeljs-exe <79073724+Angeljs-exe@users.noreply.github.com>
2021-11-01 13:26:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ console.log ("Hello!");
 console.log ("World!");
 
 console.log ("Edit dari GITHUB");
+console.log ("Test conflict lokal");
+console.log ("Test conflict");
+console.log ("Baris kelima");
